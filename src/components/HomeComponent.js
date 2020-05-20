c58381c3f519393f7074e2bfec6921608f5a1818
Maksym Arabadzhyi <arabadzhyi.tomnium@gmail.com>
2020-05-20 13:56:15 +0300

    Home and List components refactored as a functional components

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,30 +1,25 @@
-import React, {Component} from 'react'
+import React from 'react'
 import './HomeComponent.scss'
 import TodoForm from "./TodoForm/TodoForm";
 import TodoList from "./TodoList/TodoList";
 
-export default class HomeComponent extends Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className='container'>
-                <div className='content'>
-                    <div className='card'>
-                        <div className='card-title'>
-                            Todo List
-                        </div>
-                        <div className='card-body'>
-                            <TodoForm onSubmit={this.props.addTodo}/>
-                            <TodoList todoList={this.props.todoList}/>
-                            {this.props.isFetching?<div className="lds-dual-ring"/>:null}
-                        </div>
+const HomeComponent = props => {
+    return (
+        <div className='container'>
+            <div className='content'>
+                <div className='card'>
+                    <div className='card-title'>
+                        Todo List
+                    </div>
+                    <div className='card-body'>
+                        <TodoForm onSubmit={props.addTodo}/>
+                        <TodoList todoList={props.todoList}/>
+                        {props.isFetching?<div className="lds-dual-ring"/>:null}
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
+export default HomeComponent;
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,24 +1,21 @@
 import React,{Component} from 'react'
 
-export default class TodoList extends Component{
+const TodoList = props => {
 
-    constructor(props){
-        super(props)
-    }
+    const { todoList } = props;
 
-    render(){
-        const {todoList} = this.props;
-        return(
-            <div className='todo-list w-100'>
-                <ul className='list-group'>
-                    <li className='list-group-item active'>To-dos:</li>
-                    {
-                        todoList.map((item,index)=>{
-                            return <li className='list-group-item' key={index}>{item}</li>
-                        })
-                    }
-                </ul>
-            </div>
-        );
-    }
+    return(
+        <div className='todo-list w-100'>
+            <ul className='list-group'>
+                <li className='list-group-item active'>To-dos:</li>
+                {
+                    todoList.map((item,index)=>{
+                        return <li className='list-group-item' key={index}>{item}</li>
+                    })
+                }
+            </ul>
+        </div>
+    );
 }
+
+export default TodoList;
